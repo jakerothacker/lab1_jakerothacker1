a71875a5ae9cb643807a73be9856d150e0e4435e
Jake Rothacker <jakerothacker@gmail.com>
2026-06-19 11:12:03 -0400

    Created first eleven digit string and check digit string. Also compared result of furture function ouput to check digit.

--- lab1_jakerothacker1.py
+++ lab1_jakerothacker1.py
@@ -11,4 +11,14 @@ while status ==True:
         print("Invalid UPC. UPC entered was not a 12-digit number. Type 'quit' to exit.")
         continue
 
+    upc11 = upc[:-1]
+    upc12 = upc[-1]
 
+    expected_upc12 = find_UPC(upc11)
+
+    print("Expected UPC 12:", expected_upc12)
+    print("Actual UPC 12:", upc12)
+    if expected_upc12 == upc12:
+        print("UPC is valid.")
+    else:
+        print("UPC is invalid.")
